ecb46f3cbbf1b4c94e000bd234194114fdedd0cf
Abhay Vaidya <abhay-vaidya@users.noreply.github.com>
2019-05-16 10:13:07 -0400

    Fix appending posts vs refreshing

--- redux/Subreddit.js
+++ redux/Subreddit.js
@@ -3,6 +3,12 @@ export const GET_SUBREDDIT_POSTS = "reddit-client/posts/LOAD";
 export const GET_SUBREDDIT_POSTS_SUCCESS = "reddit-client/posts/LOAD_SUCCESS";
 export const GET_SUBREDDIT_POSTS_FAIL = "reddit-client/posts/LOAD_FAIL";
 
+export const GET_NEXT_SUBREDDIT_POSTS = "reddit-client/posts/LOAD_NEXT";
+export const GET_NEXT_SUBREDDIT_POSTS_SUCCESS =
+  "reddit-client/posts/LOAD_NEXT_SUCCESS";
+export const GET_NEXT_SUBREDDIT_POSTS_FAIL =
+  "reddit-client/posts/LOAD_NEXT_FAIL";
+
 // Reducer
 export default function reducer(
   state = {
@@ -13,14 +19,22 @@ export default function reducer(
 ) {
   switch (action.type) {
     case GET_SUBREDDIT_POSTS:
+    case GET_NEXT_SUBREDDIT_POSTS:
       return { ...state, loading: true };
     case GET_SUBREDDIT_POSTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        posts: action.payload.data.data.children
+      };
+    case GET_NEXT_SUBREDDIT_POSTS_SUCCESS:
       return {
         ...state,
         loading: false,
         posts: [...state.posts, ...action.payload.data.data.children]
       };
     case GET_SUBREDDIT_POSTS_FAIL:
+    case GET_NEXT_SUBREDDIT_POSTS_FAIL:
       return {
         ...state,
         loading: false,
@@ -45,7 +59,7 @@ export function getSubredditPosts(subreddit, sort) {
 
 export function getNextSubredditPosts(subreddit, sort, lastPostName) {
   return {
-    type: GET_SUBREDDIT_POSTS,
+    type: GET_NEXT_SUBREDDIT_POSTS,
     payload: {
       request: {
         url: `/r/${subreddit}/${sort}.json?after=${lastPostName}`
