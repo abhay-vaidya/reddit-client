1c979ea9c1bae21db4881f976c242212204f0429
Abhay Vaidya <abhay-vaidya@users.noreply.github.com>
2019-06-04 14:31:44 -0400

    Clean up stack navigator

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,53 +1,9 @@
-import React from "react";
-import { Platform } from "react-native";
-import { Button } from "react-native-elements";
 import { createStackNavigator } from "react-navigation";
-import { TabBarIcon } from "../components";
-
-import store from "../redux/Store";
-import { toggleTheme } from "../redux/Global";
-
-import Defaults from "../constants/Defaults";
 import { HomeScreen, PostDetailsScreen } from "../screens";
 
-const HomeStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    PostDetails: PostDetailsScreen
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Defaults.theme.primary,
-        borderBottomWidth: 0
-      },
-      headerTintColor: Defaults.theme.accent,
-      headerTitleStyle: {
-        fontWeight: "bold"
-      },
-      headerRight: (
-        <Button
-          type="clear"
-          onPress={() => store.dispatch(toggleTheme())}
-          icon={{
-            name: "brightness-medium",
-            size: 18,
-            color: Defaults.theme.accent
-          }}
-        />
-      )
-    }
-  }
-);
-
-HomeStack.navigationOptions = {
-  tabBarLabel: "Home",
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === "ios" ? `ios-home` : "md-home"}
-    />
-  )
-};
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  PostDetails: PostDetailsScreen
+});
 
 export default HomeStack;
